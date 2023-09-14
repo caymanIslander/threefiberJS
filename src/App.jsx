cc316ebc56f2baae0d914584b77d54957a135246
caymanIslander <duman.ozgur@hotmail.com>
2023-09-15 02:14:13 +0300

    init

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,32 @@
 import { Canvas } from "@react-three/fiber";
 import { Experience } from "./components/Experience";
+import { useMediaQuery } from "@react-hook/media-query";
+import './canvas.css'
 
 function App() {
+  const isMobile = useMediaQuery("(max-width: 686px)");
+
   return (
-    <Canvas shadows camera={{ position: [3, 3, 3], fov: 30 }}>
-      <color attach="background" args={["#ececec"]} />
-      <Experience />
-    </Canvas>
+    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+      <div style={{ display: "flex", alignItems: 'center', padding: '5px', backgroundColor: "#e2e2e2", width: "100%", height: "10%", justifyContent: "center" }}>
+        <img src="../textures/loky.png" style={{ width: "10%", height: "75%" }} />
+      </div>
+      <div className="container">
+        <Canvas shadows camera={{ position: [10, 10, 10], fov: 3 }} >
+          <Experience />
+        </Canvas>
+        <p>container</p>
+        <p>container</p>
+        <p>container</p>
+      </div>
+      <div className="test">
+        Abc
+      </div> 
+    </div>
   );
+  
 }
+
+
 
 export default App;
--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,13 +1,65 @@
-import { OrbitControls } from "@react-three/drei";
+import { Environment, MeshPortalMaterial, OrbitControls, RoundedBox, useTexture } from "@react-three/drei";
+import { AmbientLight } from "three";
+import * as THREE from "three"
+import { Mavi } from "./mavi";
+import { Pembe } from './pembe';
+import { useState } from "react";
+
 
 export const Experience = () => {
+const map = useTexture('../textures/texture.jpg')
+const map2 = useTexture('../textures/texture2.jpg')
+const [active, setActive] = useState(null)
+const [hovered, setHovered] = useState(null)
+
+const handleHover = () => {
+  setHovered(null)
+}
+
+//  <RoundedBox args={[2,3,0.025]} position-x={1.5}>
   return (
     <>
-      <OrbitControls />
-      <mesh>
-        <boxGeometry />
-        <meshNormalMaterial />
-      </mesh>
+    
+      <OrbitControls
+      minPolarAngle={Math.PI / 4} 
+      maxPolarAngle={(3 * Math.PI) / 6} 
+      minAzimuthAngle={-Math.PI / 4} 
+      maxAzimuthAngle={Math.PI / 4}
+      enableZoom={true} 
+      target={[0,0,0]}
+      
+      />
+      <ambientLight intensity={0.25}/>
+      <Environment preset="sunset"/>
+      <group>
+      <RoundedBox args={hovered ? [2,3,1] : [2,3,0.025]}
+       position-x={1.5}>
+        <MeshPortalMaterial side={THREE.DoubleSide}>
+          <ambientLight intensity={1}/>
+          <Environment preset="sunset"/>
+          <Mavi scale={0.45} position-y={-1} active={active} setActive={setActive}/>
+          <mesh>
+            <sphereGeometry args={[5, 64, 64]}/>
+            <meshStandardMaterial map={map2} side={THREE.BackSide}/>
+          </mesh> 
+        </MeshPortalMaterial>
+       
+      </RoundedBox>
+
+      <RoundedBox args={[2,3, 0.025]} position-x={-1.5}>
+        <MeshPortalMaterial side={THREE.DoubleSide}>
+          <ambientLight intensity={1}/>
+          <Environment preset="sunset"/>
+          <Pembe scale={0.45} position-y={-1} rotation-y={Math.PI / 64} active={active} setActive={setActive}/>
+          <mesh>
+            <sphereGeometry args={[5, 64, 64]}/>
+            <meshStandardMaterial map={map} side={THREE.BackSide}/>
+          </mesh> 
+        </MeshPortalMaterial>
+        
+      </RoundedBox>
+
+    </group>
     </>
   );
 };
